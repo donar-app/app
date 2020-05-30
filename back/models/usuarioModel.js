f6d893d00b8ad67f7a0b6d702fe112666e1963ce
abbul <abbulrodriguez@hotmail.com>
2020-05-30 15:30:47 -0300

    ads

--- back/models/usuarioModel.js
+++ back/models/usuarioModel.js
@@ -16,7 +16,57 @@ let usuario = new Schema({
         type: String,
         ref: 'Alias',
         required: [true, 'El alias es necesario']
-    }
+    },
+    email: {
+        type: String,
+        ref: 'Alias',
+        required: [true, 'El email es necesario']
+    },
+    pais: {
+        type: String,
+        ref: 'Pais',
+        required: [true, 'El pais es necesario']
+    },
+    ciudad: {
+        type: String,
+        ref: 'Ciudad',
+        required: [true, 'El ciudad es necesario']
+    },
+    direccion: {
+        type: String,
+        ref: 'Direccion',
+        required: [false, 'El direccion es necesario']
+    },
+    telefono: {
+        type: String,
+        ref: 'Telefono',
+        required: [true, 'El telefono es necesario']
+    },
+    es_fundacion: {
+        type: String,
+        ref: 'Fundacion',
+        required: [true, 'El Campo Fundacion es necesario']
+    },
+    es_acopio: {
+        type: String,
+        ref: 'Acopio',
+        required: [true, 'El Campo Acopio es necesario']
+    },
+    es_activo: {
+        type: String,
+        ref: 'Activo',
+        required: [true, 'El Campo Activo es necesario']
+    },
+    creada_en: {
+        type: String,
+        ref: 'Creado',
+        required: [true, 'El Creado es necesario']
+    },
+    actualizada_en: {
+        type: String,
+        ref: 'Actualizo',
+        required: [false, 'El alias es necesario']
+    },
 })
 
 module.exports = mongoose.model('Usuario', usuario)
--- back/routes/seguridad.js
+++ back/routes/seguridad.js
@@ -46,25 +46,31 @@ router.post('/login', asyncHandler(async (req, res) => {
  * @returns {String} Se retorna el token
  */
 router.post('/registro', asyncHandler(async (req, res) => {
-  const usuarioM = {
-    id: '123',
-    nombre: 'Abbul',
-    apellido: 'Rodriguez',
-    alias: 'aro',
-    email: 'arodirguez@hotmail.com',
-    es_receptor: true,
-    pais: 'ar',
-    ciudad: 'capital',
-    direccion: 'calle los arboles',
-    telefono: '54911555444',
-    es_fundacion: false,
-    es_acopio: false,
-    es_activo: true,
-    creada_en: '2020-01-01 12:12:12',
-    actualizada_en: '2020-05-05 12:12:12',
+
+  const passwordRandom = generateRandomString(8);
+  const clave = await bcrypt.hashSync(req.body.usuario, SALT);
+
+  const bufferUsuario = {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    alias: req.body.alias,
+    email: req.body.email,
+    clave : clave,
+    es_receptor: req.body.receptor,
+    pais: req.body.pais,
+    ciudad: req.body.ciudad,
+    direccion: req.body.direccion,
+    telefono: req.body.telefono,
+    es_fundacion: req.body.fundacion,
+    es_acopio: req.body.acopio,
+    es_activo: req.body.activo,
+    creada_en: new Date(
+      new Date().toLocaleString('es-AR', {
+        timeZone: 'America/Argentina/Buenos_Aires'
+      })
+    )
   };
 
-  const token = await handlerToken.crearToken({ id: usuarioM.id, alias: usuarioM.alias });
   await handlerToken.setTokenEnCabecera(res, token);
 
   res.json({
