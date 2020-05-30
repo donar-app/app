bdc2dbce7354d1a2cb7c85a03c685f643b676413
Ariel Villarreal <arirecate_6@hotmail.com>
2020-05-30 11:16:04 -0300

    basic structure

---
+++ front/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './assets/styles/index.css';
+import '@fortawesome/fontawesome-free/js/all';
+import 'bootstrap.native';
+
+ReactDOM.render(<App />, document.getElementById('app'));
