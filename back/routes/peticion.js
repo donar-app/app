f69725ef6e08f187b5d79e5d860e7108b8411251
Santiago Mena <santiagomenape@gmail.com>
2020-05-31 06:22:03 -0300

    Permisos peticiones

--- back/routes/peticion.js
+++ back/routes/peticion.js
@@ -3,19 +3,32 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require('../middlewares/async-handler');
-const { createPeticion, updatePeticion } = require('../controllers/peticion');
+const { getPeticion, createPeticion, updatePeticion } = require('../controllers/peticion');
+const { NotHavePermissions } = require('./../errors');
+const { getPublication } = require('../controllers/publicacion');
 
 /**
  * Crear peticion
  */
 router.post('/', asyncHandler(async (req, res, next) => {
     // obtener usuario del token
-    const usuario_id = req.body.usuario_id;
+    const usuario_id = req.body.jwt_usuario_id;
+    const publicacion_id = req.body.publicacion_id;
+    // Verificar permisos
+    const publicacion = await getPublication(publicacion_id);
+    
+    if(publicacion.anunciante_id !== usuario_id){
 
-    res.json( await createPeticion({
-        usuario_id,
-        publicacion_id: req.body.publicacion_id
-    }));
+        res.json( await createPeticion({
+            usuario_id,
+            publicacion_id
+        }));
+
+    } else {
+
+        throw new NotHavePermissions();
+
+    }
 }));
 
 /**
@@ -24,9 +37,22 @@ router.post('/', asyncHandler(async (req, res, next) => {
 router.put('/:id', asyncHandler(async (req, res, next) => {
     const { id } = req.params
     // verificar si el usuario es el dueño de la publicación
-    res.json( await updatePeticion(id, {
-        es_aceptada: req.body.es_aceptada
-    }));
+    // obtener usuario del token
+    const usuario_id = req.body.jwt_usuario_id;
+    const publicacion_id = req.body.publicacion_id;
+    // Verificar permisos
+    const peticion = await getPeticion(id);
+    const publicacion = await getPublication(peticion.publicacion_id);
+
+    if(publicacion.anunciante_id === usuario_id){
+
+        res.json( await updatePeticion(id, {
+            es_aceptada: req.body.es_aceptada
+        }));
+
+    } else {
+
+    }
 }));
 
 module.exports = router;
