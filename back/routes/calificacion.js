22db6641737443a1fef76f0958d69ae2f0e1ec50
Santiago Mena <santiagomenape@gmail.com>
2020-05-31 13:09:19 -0300

    Retornar usuario a contactarse al calificar

--- back/routes/calificacion.js
+++ back/routes/calificacion.js
@@ -6,7 +6,7 @@ const asyncHandler = require('../middlewares/async-handler');
 const { getPeticion, createPeticion, updatePeticion } = require('../controllers/peticion');
 const { NotHavePermissions } = require('./../errors');
 const { getPublication } = require('../controllers/publicacion');
-
+const { obtenerUsuario } = require('../controllers/usuarioController');
 /**
  * Calificar petición emisor
  */
@@ -17,14 +17,16 @@ router.post('/:id/emisor', asyncHandler(async (req, res, next) => {
     const usuario_id = req.body.jwt_usuario_id;
     
     // Verificar permisos
-    const peticion = await getPeticion(id);
-    const publicacion = await getPublication(publicacion_id);
-    if(String(usuario_id) === String(publicacion_id.anunciante_id)) {
-
-        res.json( await updatePeticion(id, {
+    let peticion = await getPeticion(id);
+    const publicacion = await getPublication(peticion.publicacion_id);
+    const usuario = await obtenerUsuario(publicacion.anunciante_id);
+    
+    if(String(usuario_id) === String(publicacion.anunciante_id)) {
+        peticion = await updatePeticion(id, {
             es_entregada: req.body.es_entregada,
             calificacion_emisor: req.body.calificacion_emisor,
-        }));
+        });
+        return res.json({peticion});
 
     } else {
 
@@ -44,14 +46,16 @@ router.post('/:id/receptor', asyncHandler(async (req, res, next) => {
     const usuario_id = req.body.jwt_usuario_id;
     
     // Verificar permisos
-    const peticion = await getPeticion(id);
+    let peticion = await getPeticion(id);
+    const publicacion = await getPublication(peticion.publicacion_id);
+    const usuario = await obtenerUsuario(publicacion.anunciante_id);
 
     if(String(usuario_id) === String(peticion.usuario_id)) {
-
-        res.json( await updatePeticion(id, {
+        peticion = await updatePeticion(id, {
             es_recibida: req.body.es_recibida,
             calificacion_receptor: req.body.calificacion_receptor,
-        }));
+        });
+        res.json({peticion});
 
     } else {
 
