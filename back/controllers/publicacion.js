4da9f07c4521439ec1bc496dea48505219e4e197
abbul <abbulrodriguez@hotmail.com>
2020-05-31 17:22:37 -0300

    asd

--- back/controllers/publicacion.js
+++ back/controllers/publicacion.js
@@ -14,7 +14,7 @@ const getAllPublications = async () => {
 
     for await (const publi of resp) {
 
-        publi.imagenRoute = await fs.readFileSync(publi.imagenRoute, 'base64')
+        publi.imagenRoute = await fs.readFileSync(path.resolve( __dirname, `../uploads/${publi.imagenRoute}.png`), 'base64')
        // publi.imagenRoute = await createImage(publi.imagenRoute);
     }
 
@@ -28,7 +28,7 @@ const getPublication = async (id) => {
         let resp = await Publicacion.findById( id );
 
         //resp.imagenRoute = await createImage(resp.imagenRoute);
-        resp.imagenRoute = await fs.readFileSync(publi.imagenRoute, 'base64')
+        resp.imagenRoute = await fs.readFileSync(path.resolve( __dirname, `../uploads/${publi.imagenRoute}.png`), 'base64')
         return resp;
         
 	} catch (e) {
@@ -43,8 +43,8 @@ const createPublication = async ( publicacion ) => {
     const { jwt_usuario_id : id, titulo, categoria, descripcion, tipo, imagenRoute } = publicacion;
     //let imagen =  Buffer.from(imagenRoute, 'base64');
     let nameFile = `${id}-${new Date().getTime()}`;
-    let buff = new Buffer(imagenRoute, 'base64');
-    await fs.writeFileSync(`${nameFile}.png`, buff);
+    //let buff = new Buffer(imagenRoute, 'base64');
+    await fs.writeFileSync(path.resolve( __dirname, `../uploads/${nameFile}.png`), imagenRoute);
 
     //if ( !isImage( imagen ) ) throw new ResourceNotImage();
 
