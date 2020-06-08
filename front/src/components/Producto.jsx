60f5f164ba5a8d2e2988371578d449e6ca1a1799
Santiago Mena <santiagomenape@gmail.com>
2020-06-08 10:19:57 -0300

    Estilos

--- front/src/components/Producto.jsx
+++ front/src/components/Producto.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Producto = ({ image, alt, name }) => {
   return (
-    <div className='tw-flex tw-flex-col tw-justify-center tw-items-center tw-border-2 tw-rounded'>
-      <img className='tw-object-contain tw-object-center tw-rounded-full tw-h-16 tw-w-16 tw-p-1 shadow-left' src={image} alt={alt} />
+    <div className='tw-flex tw-flex-col tw-justify-center tw-items-center tw-rounded'>
+      <img className='tw-object-contain tw-object-center tw-rounded-full tw-h-32 tw-w-32 tw-p-1 shadow-left tw-bg-white' src={image} alt={alt} />
       <div className='tw-text-center tw-leading-snug tw-p-1 tw-w-full'>
         <h3 className='tw-font-bold tw-text-gray-800 tw-text-sm'>{name}</h3>
         <p className='tw-font-bold tw-text-xs text-orange-donar'>Más detalles</p>
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -38,18 +38,18 @@ const Home = ({ authorization }) => {
   return (
     <main className='animate__animated animate__fadeIn'>
       <div className='tw-relative'>
-        <img className='tw-object-cover tw-h-40 md:tw-h-64 tw-w-full opacity-80' src={donando} alt='' />
+        <img className='tw-object-cover tw-h-16 md:tw-h-40 tw-w-full opacity-80' src={donando} alt='' />
         <div className='tw-absolute tw-px-5 tw-inset-0 tw-flex tw-flex-col tw-justify-center tw-items-center'>
           <p className='tw-pb-3 tw-font-bold tw-text-lg tw-text-white md:tw-text-4xl text-shadow'>
-            Publicá lo que tenes para donar o elegí en las solicitudes
+            Dona o solicita lo que (no) necesites!
           </p>
-          <div className='tw-text-right tw-w-full md:tw-text-center'>
+          {/* <div className='tw-text-right tw-w-full md:tw-text-center'>
             <ButtonPill bgColor='tw-bg-white' textColor='text-orange-donar' px='tw-px-6' py='tw-py-0' textSize='tw-text-2xl' handleClick={() => goTo('/publicar/donar')}>Donar</ButtonPill>
-          </div>
+          </div> */}
         </div>
       </div>
-      <SliderProduct smallText='Recibé una donación' productos={!Array.isArray(productos) ? null : productos.filter((producto) => { return producto.tipo === "Donación"; })}>Donaciones</SliderProduct>
-      <SliderProduct smallText='Solicita una donación' productos={!Array.isArray(productos) ? null : productos.filter((producto) => { return producto.tipo === "Solicitud"; })}>Solicitudes de donaciones</SliderProduct>
+      <SliderProduct smallText='Recibé una donación' productos={!Array.isArray(productos) ? null : productos.filter((producto) => { return producto.tipo === 'Donación'; })}>Donaciones</SliderProduct>
+      <SliderProduct smallText='Solicita una donación' productos={!Array.isArray(productos) ? null : productos.filter((producto) => { return producto.tipo === 'Solicitud'; })}>Solicitudes de donaciones</SliderProduct>
     </main>
   );
 };
