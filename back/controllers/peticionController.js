47f61040a5617910b408f3b9f7be0d97918dbf6c
Santiago Mena <santiagomenape@gmail.com>
2020-07-11 18:34:59 -0300

    fix

--- back/controllers/peticionController.js
+++ back/controllers/peticionController.js
@@ -25,7 +25,11 @@ const crearPeticion = asyncHandler(async (req, res) => {
     new Date().toLocaleString('es-AR', {
       timeZone: 'America/Argentina/Buenos_Aires'
     }))
-  const peticion = await PeticionRepository.guardar(objPeticion)
+  const peticion = await PeticionRepository.guardar({
+    usuario_id: usuarioID,
+    publicacion_id: publicacion.id,
+    creado_en: objPeticion.creado_en
+  })
   if (!peticion) {
     return res.json(responseJSON(false, 'peticion-error', 'No se puede guardar la peticion', []))
   }
